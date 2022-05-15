c49df16d8b34336f20129c4193aaade45323d1b9
FilipRajcic <filiprajcic7@gmail.com>
2022-05-16 00:09:18 +0200

    Fixed BooksFetchLink

--- src/api/books.js
+++ src/api/books.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // Setting up axios for books ajax call
 export default axios.create({
-  baseURL: "http://openlibrary.org",
+  baseURL: "https://openlibrary.org",
   params: {
     limit: 10,
   },
